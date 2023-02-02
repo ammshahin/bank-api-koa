137595ca5aee28daf87d02a21ce62ae761a7d74d
ammshahin <ammshahin@gmail.com>
2023-02-02 17:27:35 +0600

    mongoose added

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const  Koa =require("koa");
 const {koaBody} =require("koa-body");
 const  KoaLogger =require("koa-logger");
 const cors =require("koa2-cors");
+const mongoose = require("mongoose")
 const router =require("./routes");
+const { appPort, mongoDbSRV } = require("./variables");
 
-const appPort = process.env.PORT;
+mongoose.connect(mongoDbSRV,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async() => {
+    console.log("Database connected!");
+  })
+  .catch(err => console.log(err))
 
 const app = new Koa();
 
@@ -18,7 +27,7 @@ app.use(KoaLogger());
 app.use(router.routes());
 
 const server = app.listen(appPort, ()=>{
-    console.log(`Server is listening to http://localhost${appPort}`);
+    console.log(`Server is listening to http://localhost:${appPort}`);
 })
 
 module.exports = server;
---
+++ variables/index.js
@@ -0,0 +1,2 @@
+exports.appPort = process.env.PORT || 5011;
+exports.mongoDbSRV = "mongodb+srv://ammshahin:12345@test-bank-db.o4h3gmz.mongodb.net/?retryWrites=true&w=majority";
